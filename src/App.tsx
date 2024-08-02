ca29977333a784ac113fe9a3ada11e28b68a00d8
RJD02 <dulangeraviraj@gmail.com>
2024-08-02 21:28:58 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  const router = createBrowserRouter([
+  {
+    'path': '/',
+    element: <Home />
+  }
+
+  ])
+
+  return (
+    <>
+    <Navbar />
+    <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+  <nav>
+    <h1>Navbar</h1>
+    <div className="links">
+      <a href="/">Home</a>
+      <a href="/create">New Blog</a>
+    </div>
+  </nav>
+  )
+}
+
