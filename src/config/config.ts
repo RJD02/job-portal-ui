52cf4b2f23f9b85294672d016466f604a45653f1
AnishRajwani <anish.rajwani@whiteklay.in>
2025-01-01 14:25:09 +0530

    added new backend dependency

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const serverUrl = import.meta.env.VITE_ENV === "DEVELOPMENT" ? "http://localhost:5000" : "https://job-portal-go-ntuh.onrender.com";
+export const serverUrl = import.meta.env.VITE_ENV === "DEVELOPMENT" ? "http://localhost:5000" : "https://job-portal-python.onrender.com";
 
 export const ADMIN_KEY = import.meta.env.VITE_ADMIN_KEY;
 
