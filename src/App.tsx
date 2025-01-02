0ba340a989ca60d36a112ba4c6efe199b7adc7fe
AnishRajwani <anish.rajwani@whiteklay.in>
2025-01-02 13:35:31 +0530

    resovled home page issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
+// import Home from './pages/Home'
 import Jobs from './pages/job/Jobs'
 import Job from './pages/job/Job'
 import Auth from './pages/Auth/Auth'
