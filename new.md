2a34781d63349d2a3cb7700660b23e5b6aa125b6
RJD02 <dulangeraviraj@gmail.com>
2025-01-05 12:24:48 +0530

    Update

---
+++ new.md
@@ -0,0 +1,12 @@
+# Anish's changes
+
+
+Ijjat se home page me jobs dikhne chahiye
+Signup access should be after login
+
+Click here
+Login
+Jobs bina login ke dikhne chahiye
+Job card replace shortDescription with batch
+Add job - img url, batch as input
+Google ads testing
--- src/pages/AddJob/AddJob.tsx
+++ src/pages/AddJob/AddJob.tsx
@@ -49,7 +49,7 @@ export default function AddJobPage() {
                         shortDescription: '',
                         salary,
                         deadline,
-                        applicationLink
+                        applyLink: applicationLink
                     })
                 })
 
