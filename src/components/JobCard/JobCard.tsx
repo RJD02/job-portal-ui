3d863c1e8597a1c483ad9020264b630cece47ebf
AnishRajwani <anish.rajwani@whiteklay.in>
2025-01-05 20:38:53 +0530

    changed the view and added description and location

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -11,13 +11,17 @@ export const JobCard = (props: { job: JobType }) => {
                 <div className="p-6 bg-white border-2 border-black rounded-lg shadow-md">
                     <div className="flex items-center justify-between">
                         <h2 className="text-xl font-semibold text-black">{j.designation}</h2>
-                        <span className="px-3 py-1 text-sm font-medium text-white bg-black rounded-full">Remote</span>
+                        <span className="px-3 py-1 text-sm font-medium text-white bg-black rounded-full">{j.location}</span>
                     </div>
-                    <p className="mt-2 text-gray-700">{j.batch}</p>
+                    <p className="font-semibold text-black">{j.batch}</p>
 
                     <div className="mt-4">
                         <span className="block text-gray-500 text-sm">Company:</span>
                         <p className="font-semibold text-black">{j.companyName}</p>
+                    </div>
+                    <div className="mt-4">
+                        <span className="block text-gray-500 text-sm">Description:</span>
+                        <p className="truncate mt-2 text-gray-700 text-ellipses overflow-hidden">{j.description}</p>
                     </div>
 
                     <div className="flex items-center justify-between mt-4">
